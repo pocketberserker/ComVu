fefadef4bc5f5faf763fec79d48ba93b4cf0a000
pocketberserker <pocketberserker@gmail.com>
2016-07-07 06:55:12 +0900

    pretty print

--- src/ComVu.Core/Types.fs
+++ src/ComVu.Core/Types.fs
@@ -1,6 +1,15 @@
 ﻿namespace ComVu
 
 open System
+open YC.PrettyPrinter.Pretty
+open YC.PrettyPrinter.StructuredFormat
+
+[<AutoOpen>]
+module private FormatHelper =
+
+  let bracketL l = wordL "(" -- l @@ wordL ")"
+
+  let methodArgs xs = bracketL (sepListL (wordL ",") xs)
 
 type ComputationExpressionBody =
   | Const of string
@@ -19,41 +28,34 @@ type ComputationExpressionBody =
   | Delay of string * ComputationExpressionBody
   | Run of string * ComputationExpressionBody
 with
-  override this.ToString() =
+  member this.Doc =
     match this with
     | Const v
-    | Value v -> v
+    | Value v -> wordL v
     | NewObject args ->
       let args =
         args
         |> List.map (fun x -> x.ToString())
-        |> String.concat ("," + Environment.NewLine)
-      if String.IsNullOrEmpty(args) || args = "()" then "()"
-      else sprintf "(%s)" args
-    | Return(instance, arg) -> sprintf "%s.Return(%O)" instance arg
-    | ReturnBang(instance, arg) -> sprintf "%s.ReturnFrom(%O)" instance arg
-    | Yield(instance, arg) -> sprintf "%s.Yield(%O)" instance arg
-    | YieldBang(instance, arg) -> sprintf "%s.YieldFrom(%O)" instance arg
-    | Lambda(arg, body) -> sprintf "fun %s ->%s%O" arg Environment.NewLine body
-    | Let(name, value, body) -> sprintf "let %s = %O in %O" name value body
-    | ExpressionCall(instance, name, args) ->
-      let receiver =
-        match instance with
-        | Some i -> i.ToString() + "."
-        | None -> ""
-      let args =
-        args
-        |> List.map (fun x -> x.ToString())
-        |> String.concat ("," + Environment.NewLine)
-      let args = if String.IsNullOrEmpty(args) then "" else args + Environment.NewLine
-      sprintf "%s%s(%s)" receiver name args
+      if List.isEmpty args || args = ["()"] then bracketL emptyL
+      else args |> List.map wordL |> tupleL
+    | Return(instance, arg) -> wordL instance ^^ wordL ".Return" ^^ methodArgs [arg.Doc]
+    | ReturnBang(instance, arg) -> wordL instance ^^ wordL ".ReturnFrom" ^^ methodArgs [arg.Doc]
+    | Yield(instance, arg) -> wordL instance ^^ wordL ".Yield" ^^ methodArgs [arg.Doc]
+    | YieldBang(instance, arg) -> wordL instance ^^ wordL ".YieldFrom" ^^ methodArgs [arg.Doc]
+    | Lambda(arg, body) -> wordL "fun" ^^ wordL arg ^^ wordL "->" -- body.Doc
+    | Let(name, value, body) -> wordL "let" ^^ wordL name ^^ wordL "=" ^^ value.Doc @@ body.Doc
+    | ExpressionCall(receiver, name, args) ->
+      let receiver = match receiver with | Some x -> x.ToString() + "." |> wordL | None -> emptyL
+      let args = args |> List.map objL |> methodArgs
+      receiver ^^ wordL name ^^ args
     | LetBang(instance, src, lambda) ->
-      sprintf "%s.Bind(%s%O,%s%O%s)" instance Environment.NewLine src Environment.NewLine lambda Environment.NewLine
+      wordL instance ^^ wordL ".Bind" ^^ methodArgs [src.Doc; lambda.Doc]
     | Use(instance, src, lambda) ->
-      sprintf "%s.Using(%s%O,%s%O%s)" instance Environment.NewLine src Environment.NewLine lambda Environment.NewLine
-    | Quote(expr) -> sprintf "<@%s%O%s@>" Environment.NewLine expr Environment.NewLine
-    | Delay(instance, expr) -> sprintf "%s.Delay(%s%O%s)" instance Environment.NewLine expr Environment.NewLine
-    | Run(instance, expr) -> sprintf "%s.Run(%s%O%s)" instance Environment.NewLine expr Environment.NewLine
+      wordL instance ^^ wordL ".Using" ^^ methodArgs [src.Doc; lambda.Doc]
+    | Quote(expr) -> wordL "<@" -- expr.Doc @@ wordL "@>"
+    | Delay(instance, expr) -> wordL instance ^^ wordL ".Delay" ^^ methodArgs [expr.Doc]
+    | Run(instance, expr) -> wordL instance ^^ wordL ".Run" ^^ methodArgs [expr.Doc]
+  override this.ToString() = print 2 this.Doc
 
 type ComputationExpression = {
   Instance: string
@@ -61,9 +63,8 @@ type ComputationExpression = {
   Body: ComputationExpressionBody
 }
 with
-  override this.ToString() =
-    [ sprintf "(fun %s ->" this.Arg; this.Body.ToString(); sprintf ") %s" this.Instance ]
-    |> String.concat Environment.NewLine
+  member this.Doc = wordL "(fun" ^^ wordL this.Arg ^^ wordL "->" -- this.Body.Doc @@ wordL ")" ^^ wordL this.Instance
+  override this.ToString() = print 2 this.Doc
 
 type AnalysisResult<'T> =
   | Success of 'T
